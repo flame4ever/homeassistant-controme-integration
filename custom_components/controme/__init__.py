6f4e43b2fc6aee8d74bf250110d20e82e486cd21
flame4ever <zweiradb@gmx.de>
2025-02-13 16:47:29 +0100

    fix

--- custom_components/controme/__init__.py
+++ custom_components/controme/__init__.py
@@ -5,8 +5,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 
-# Da diese Integration ausschließlich über config entries eingerichtet wird,
-# verwenden wir das passende leere Schema.
 CONFIG_SCHEMA = cv.config_entry_only_config_schema
 
 from .const import DOMAIN, PLATFORMS
